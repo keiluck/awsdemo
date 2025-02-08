e07dc08aaf7a7c30eb9113c1639708684062dc8d
keiluck <keiluck365@gmail.com>
2025-02-09 00:34:33 +0900

    11

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
       return [
         {
           source: '/',
-          destination: '/demo',
+          destination: '/',
           permanent: true,
         },
       ];
