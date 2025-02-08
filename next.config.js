30dd69675601a98df8b2879c1ba40a08d2dc3cf4
keiluck <keiluck365@gmail.com>
2025-02-09 01:57:11 +0900

    33

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 const nextConfig = {
     transpilePackages: ['antd-mobile'],
     // output: "export",  // 让 Next.js 生成静态 HTML 文件
-    // images: {
-    //   unoptimized: true,  // 解决 Amplify 不支持 Image Optimization 问题
-    // },
+    images: {
+      unoptimized: true,  // 解决 Amplify 不支持 Image Optimization 问题
+    },
 }
 
   module.exports = nextConfig;
